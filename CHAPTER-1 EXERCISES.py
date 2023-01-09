51514a2a529e6b3b56d722f40d82d9007348b0e1
L7teef <119158458+L7teef@users.noreply.github.com>
2023-01-09 11:10:44 +0400

    Add files via upload

---
+++ CHAPTER-1 EXERCISES.py
@@ -0,0 +1,27 @@
+#1. Write a Python program to print the following string in a specific format.Twinkle, twinkle, little star, How I wonder what you are! 
+#   Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you 
+#print("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are")
+
+#2. Write a program to print the version of python
+# import sys
+# print('Python version')
+# print(sys.version)
+# print('Version info.')
+# print(sys.version_info)
+
+#3. Write a program to print the date and time
+# from datetime import datetime
+# now = datetime.now()
+# print("now =", now)
+# dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+# print("date and time =", dt_string)
+
+#4.Write three strings in different variables and print the output as one string.
+# First_name = "Aiman"
+# Middle_name = "Hossain"
+# Last_name = "Chowdhury"
+# print("Hi",First_name , Middle_name , Last_name , "nice to meet you")
+
+#5. Write a Python program which accepts the radius of a circle from the user and compute the area.
+# radius = float(input("Enter the radius of the circle : "))
+# print ("Area of the circle is: " + str(3.14* radius**2))
